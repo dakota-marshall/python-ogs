2138af7b2e197809d673a30bb62dbfaacb3d3e5e
Dakota Marshall <me@dakotamarshall.net>
2023-03-06 20:03:31 +0000

    Add same code as importable Class

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Note: You need to replace your_client_id, your_client_secret, your_username, you
 Also, note that this code sends a challenge to the opponent by creating a dictionary with the is_ranked, opponent, and game_settings fields, and sending it to the server using the requests.post() method. Once the opponent accepts the challenge, the server sends a message with the game_state field set to 'play', indicating that the game has started. At this point, the code prints the board using a custom print_board() function (which you need to define), and sends a move.
 
 You'll need to replace the dummy vertex 'D4' with the actual vertex you want to play, based on your strategy. Also, you'll need to define the print_board() function to print the board in a human-readable format.
+
+`ogs_client.py`
+
+```python
+from ogs_client import OGSClient
+
+client = OGSClient('your_client_id', 'your_client_secret', 'your_username', 'your_password')
+client.start_game('opponent_username')
+```
---
+++ ogs_client.py
@@ -0,0 +1,67 @@
+import requests
+import websocket
+import json
+
+class OGSClient:
+    def __init__(self, client_id, client_secret, username, password):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.username = username
+        self.password = password
+        self.access_token = None
+
+    def authenticate(self):
+        url = 'https://online-go.com/oauth2/access_token/'
+        response = requests.post(url, data={
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'password',
+            'username': self.username,
+            'password': self.password
+        })
+        self.access_token = response.json()['access_token']
+
+    def send_challenge(self, player_username):
+        game_settings = {
+            'board_size': 19,
+            'handicap': 0,
+            'komi': 6.5,
+            'time_control': 'fischer',
+            'time_control_parameters': {'main_time': 600, 'fischer_increment': 30}
+        }
+        url = 'https://online-go.com/api/v1/games/'
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        data = {'is_ranked': False, 'opponent': player_username, 'game_settings': game_settings}
+        response = requests.post(url, headers=headers, json=data)
+        game_id = response.json()['id']
+        return game_id
+
+    def play_game(self, game_id):
+        url = f'wss://online-go.com/api/v1/ws/game/{game_id}/'
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+
+        def on_message(ws, message):
+            """Handle incoming messages from the server."""
+            data = json.loads(message)
+            if 'game_state' in data and data['game_state'] == 'play':
+                # If the game has started, print the board and send a move.
+                board = data['moves'][-1]['position']
+                print_board(board)
+                move = {'type': 'play', 'color': 'B', 'vertex': 'D4'}
+                ws.send(json.dumps(move))
+            elif 'move_requested' in data:
+                # If the server requests a move, send a move.
+                move = {'type': 'play', 'color': data['move_requested']['color'], 'vertex': 'D4'}
+                ws.send(json.dumps(move))
+
+        ws = websocket.WebSocketApp(url, on_message=on_message, header=headers)
+        ws.run_forever()
+
+    def print_board(self, board):
+        for i in range(len(board)):
+            print(' '.join(board[i]))
+
+    def start_game(self, opponent_username):
+        self.authenticate()
+        game_id = self.send_challenge(opponent_username)
+        self.play_game(game_id)
