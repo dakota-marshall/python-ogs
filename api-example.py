9e785d5df1647de1a90a7353c1c35f7a3b2b0def
Dakota Marshall <me@dakotamarshall.net>
2023-03-06 19:41:56 +0000

    Add basic example

---
+++ api-example.py
@@ -0,0 +1,55 @@
+import requests
+import websocket
+import json
+
+# Authentication
+url = 'https://online-go.com/oauth2/access_token/'
+client_id = 'your_client_id'
+client_secret = 'your_client_secret'
+username = 'your_username'
+password = 'your_password'
+
+response = requests.post(url, data={
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'grant_type': 'password',
+    'username': username,
+    'password': password
+})
+access_token = response.json()['access_token']
+
+# Send a challenge to a player
+player_username = 'opponent_username'
+game_settings = {
+    'board_size': 19,
+    'handicap': 0,
+    'komi': 6.5,
+    'time_control': 'fischer',
+    'time_control_parameters': {'main_time': 600, 'fischer_increment': 30}
+}
+url = 'https://online-go.com/api/v1/games/'
+headers = {'Authorization': f'Bearer {access_token}'}
+data = {'is_ranked': False, 'opponent': player_username, 'game_settings': game_settings}
+response = requests.post(url, headers=headers, json=data)
+game_id = response.json()['id']
+
+# Join the game when the opponent accepts the challenge
+url = f'wss://online-go.com/api/v1/ws/game/{game_id}/'
+headers = {'Authorization': f'Bearer {access_token}'}
+
+def on_message(ws, message):
+    """Handle incoming messages from the server."""
+    data = json.loads(message)
+    if 'game_state' in data and data['game_state'] == 'play':
+        # If the game has started, print the board and send a move.
+        board = data['moves'][-1]['position']
+        print_board(board)
+        move = {'type': 'play', 'color': 'B', 'vertex': 'D4'}
+        ws.send(json.dumps(move))
+    elif 'move_requested' in data:
+        # If the server requests a move, send a move.
+        move = {'type': 'play', 'color': data['move_requested']['color'], 'vertex': 'D4'}
+        ws.send(json.dumps(move))
+
+ws = websocket.WebSocketApp(url, on_message=on_message, header=headers)
+ws.run_forever()
