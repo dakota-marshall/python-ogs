79382fa7f052990e59629f996c988ce140104b02
dakota.marshall <me@dakotamarshall.net>
2023-03-07 19:50:41 -0500

    Add Error Checking and endpoints Add Exception handling Class and error checking for requests Added More user endpoint functions Moved functions around into categories

--- ogs_client.py
+++ ogs_client.py
@@ -3,6 +3,9 @@ import urllib.parse
 import socketio
 import json
 
+class OGSApiException(Exception):
+    pass
+
 class OGSClient:
     def __init__(self, client_id, client_secret, username, password):
         self.client_id = client_id
@@ -16,29 +19,47 @@ class OGSClient:
         self.api_ver = "v1"
 
         # Attempt authentication once everything is defined
+        # TODO: Maybe implement some form of token caching, so we arent making new tokens every time the script runs
         self.authenticate()
 
+    # TODO: All these internal functions should be moved into private functions
     def authenticate(self):
         endpoint = f'{self.base_url}/oauth2/token/'
-        response = requests.post(endpoint, data={
-            'client_id': self.client_id,
-            'grant_type': 'password',
-            'username': self.username,
-            'password': self.password
-        }, headers={'Content-Type': 'application/x-www-form-urlencoded'})
-        # Save Access Token, Refresh Token, and User ID
-        self.access_token = response.json()['access_token']
-        print(self.access_token)
-        self.refresh_token = response.json()['refresh_token']
-        self.user_id = self.user_vitals()['id']
+        try:
+            response = requests.post(endpoint, data={
+                'client_id': self.client_id,
+                'grant_type': 'password',
+                'username': self.username,
+                'password': self.password
+            }, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        except requests.exceptions.RequestException as e:
+            raise OGSApiException("Authentication Failed") from e
 
+        if 299 >= response.status_code >= 200:
+            # Save Access Token, Refresh Token, and User ID
+            # TODO: This should probably be made into a user object that has token and ID info
+            self.access_token = response.json()['access_token']
+            print(self.access_token)
+            self.refresh_token = response.json()['refresh_token']
+            self.user_id = self.user_vitals()['id']
+        else:
+            raise OGSApiException(f"{response.status_code}: {response.reason}")
+
+    # TODO: The GET POST and PUT functions can be refactored into its own class, because DRY
     def get_rest_endpoint(self, endpoint: str, params: dict = None):
         url = f'{self.base_url}/api/{self.api_ver}{endpoint}'
         headers = {
             'Authorization' : f'Bearer {self.access_token}'
         }
+        try:
             response = requests.get(url, headers=headers, params=params)
-        return response.json()
+        except requests.exceptions.RequestException as e:
+            raise OGSApiException("GET Failed") from e
+
+        if 299 >= response.status_code >= 200:
+            return response.json()
+        else:
+            raise OGSApiException(f"{response.status_code}: {response.reason}")
 
     def post_rest_endpoint(self, endpoint: str, payload: dict, params: dict = None):
         url = f'{self.base_url}/api/{self.api_ver}{endpoint}'
@@ -46,41 +67,98 @@ class OGSClient:
             'Authorization' : f'Bearer {self.access_token}',
             'Content-Type': 'application/json'
         }
+        try:
             response = requests.post(url, headers=headers, json=payload, params=params)
-        return response.json()
+        except requests.exceptions.RequestException as e:
+            raise OGSApiException("POST Failed") from e
+
+        if 299 >= response.status_code >= 200:
+            return response.json()
+        else:
+            raise OGSApiException(f"{response.status_code}: {response.reason}")
+
+    def put_rest_endpoint(self, endpoint: str, payload: dict, params: dict = None):
+        url = f'{self.base_url}/api/{self.api_ver}{endpoint}'
+        headers = {
+            'Authorization' : f'Bearer {self.access_token}',
+            'Content-Type': 'application/json'
+        }
+        try:
+            response = requests.put(url, headers=headers, json=payload, params=params)
+        except requests.exceptions.RequestException as e:
+            raise OGSApiException("PUT Failed") from e
+
+        if 299 >= response.status_code >= 200:
+            return response.json()
+        else:
+            raise OGSApiException(f"{response.status_code}: {response.reason}")
+
+    def delete_rest_endpoint(self, endpoint: str, payload: dict, params: dict = None):
+        url = f'{self.base_url}/api/{self.api_ver}{endpoint}'
+        headers = {
+            'Authorization' : f'Bearer {self.access_token}',
+            'Content-Type': 'application/json'
+        }
+        try:
+            response = requests.delete(url, headers=headers, json=payload, params=params)
+        except requests.exceptions.RequestException as e:
+            raise OGSApiException("DELETE Failed") from e
+
+        if 299 >= response.status_code >= 200:
+            return response.json()
+        else:
+            raise OGSApiException(f"{response.status_code}: {response.reason}")
+
+# User Specific Resources: /me
 
     def user_vitals(self):
         endpoint = '/me'
         return self.get_rest_endpoint(endpoint)
+    
+    def user_settings(self):
+        endpoint = '/me/settings/'
+        return self.get_rest_endpoint(endpoint=endpoint)
+    
+    def update_user_settings(self, payload: dict = None):
+        endpoint = '/me/settings/'
+        payload = {
+            'website' : 'https://example.com'
+        }
+        return self.put_rest_endpoint(endpoint=endpoint, payload=payload)
+
+    def user_games(self):
+        endpoint = '/me/games'
+        return self.get_rest_endpoint(endpoint)
+
+    def user_friends(self, username: str = None):
+        endpoint = '/me/friends'
+        return self.get_rest_endpoint(endpoint=endpoint, params={'username' : username})
+
+    def send_friend_request(self, username: str):
+        endpoint = '/me/friends'
+        player_id = self.get_player(username)['id']
+        payload = {
+            "player_id" : player_id
+        }
+        return self.post_rest_endpoint(endpoint=endpoint, payload=payload)
+
+    # TODO: This method doesnt work, DELETE is not allowed on this endpoint
+    def remove_friend(self, username: str):
+        endpoint = '/me/friends/'
+        player_id = self.get_player(username)['id']
+        payload = {
+            "player_id" : player_id
+        }
+        return self.delete_rest_endpoint(endpoint=endpoint, payload=payload)
+
+# Players: /players
 
     def get_player(self, player_username):
         encoded_name = urllib.parse.quote(player_username)
         endpoint = f'/players/'
         return self.get_rest_endpoint(endpoint=endpoint, params={'username' : encoded_name})['results'][0]
 
-    def received_challenges(self):
-        endpoint = '/me/challenges'
-        received_challenges = []
-        all_challenges = self.get_rest_endpoint(endpoint)['results']
-        for challenge in all_challenges:
-            if challenge['challenger']['id'] != self.user_id:
-                received_challenges.append(challenge)
-        return received_challenges
-
-    def sent_challenges(self):
-        endpoint = '/me/challenges'
-        sent_challenges = []
-        all_challenges = self.get_rest_endpoint(endpoint)['results']
-        for challenge in all_challenges:
-            if challenge['challenger']['id'] == self.user_id:
-                sent_challenges.append(challenge)
-        return sent_challenges
-
-    def accept_challenge(self, challenge_id):
-        endpoint = f'/me/challenges/{challenge_id}/accept'
-        return self.get_rest_endpoint(endpoint)
-
-# TODO: Need to make these customizable 
+    # TODO: Need to make these customizable 
     def challenge_player(self, player_username):
         game_settings = {
             "initialized": False,
@@ -91,8 +169,8 @@ class OGSClient:
                 "name": "test",
                 "rules": "japanese",
                 "ranked":False,
-                "width":19,
-                "height":19,
+                "width":9,
+                "height":9,
                 "handicap": "0",
                 "komi_auto": "custom",
                 "komi":0,
@@ -102,16 +180,16 @@ class OGSClient:
                 "time_control": "byoyomi",
                 "time_control_parameters": {
                     "system": "byoyomi",
-                    "speed": "60",
-                    "total_time":60,
-                    "initial_time":60,
-                    "time_increment":60,
-                    "max_time":60,
-                    "main_time":60,
-                    "period_time":60,
-                    "periods":60,
-                    "per_move":60,
-                    "stones_per_period":60,
+                    "speed": "60000",
+                    "total_time":60000,
+                    "initial_time":60000,
+                    "time_increment":60000,
+                    "max_time":60000,
+                    "main_time":660000,
+                    "period_time":60000,
+                    "periods":60000,
+                    "per_move":60000,
+                    "stones_per_period":10,
                     "pause_on_weekends": False,
                     "time_control": "byoyomi"
                 },
@@ -126,43 +204,43 @@ class OGSClient:
         challenge_id = response['challenge']
         game_id = response['game']
         return challenge_id, game_id
-        
-    def user_games(self):
-        endpoint = '/me/games'
+
+# Challenges
+
+    # TODO: Change these to use the 'challenger' parameter instead of looping through all challenges
+    def received_challenges(self):
+        endpoint = '/me/challenges/'
+        received_challenges = []
+        all_challenges = self.get_rest_endpoint(endpoint)['results']
+        for challenge in all_challenges:
+            if challenge['challenger']['id'] != self.user_id:
+                received_challenges.append(challenge)
+        return received_challenges
+
+    # TODO: Same as above
+    def sent_challenges(self):
+        endpoint = '/me/challenges'
+        sent_challenges = []
+        all_challenges = self.get_rest_endpoint(endpoint)['results']
+        for challenge in all_challenges:
+            if challenge['challenger']['id'] == self.user_id:
+                sent_challenges.append(challenge)
+        return sent_challenges
+    
+    # TODO: Needs Tested
+    def accept_challenge(self, challenge_id):
+        endpoint = f'/me/challenges/{challenge_id}/accept'
         return self.get_rest_endpoint(endpoint)
+    
+    # TODO: Needs Tested
+    def decline_challenge(self, challenge_id):
+        endpoint = f'/me/challenges/{challenge_id}/decline'
+        return self.get_rest_endpoint(endpoint=endpoint)
+
+    def challenge_details(self, challenge_id: int):
+        endpoint = f'/me/challenges/{challenge_id}'
+        return self.get_rest_endpoint(endpoint=endpoint)
 
     def game_details(self, game_id):
         endpoint = f'/games/{game_id}'
         return self.get_rest_endpoint(endpoint)
-
-
-# -- Needs testing --
-    # def play_game(self, game_id):
-
-    #     headers = {'Authorization': f'Bearer {self.access_token}'}
-
-    #     def on_message(ws, message):
-    #         """Handle incoming messages from the server."""
-    #         data = json.loads(message)
-    #         if 'game_state' in data and data['game_state'] == 'play':
-    #             # If the game has started, print the board and send a move.
-    #             board = data['moves'][-1]['position']
-    #             print_board(board)
-    #             move = {'type': 'play', 'color': 'B', 'vertex': 'D4'}
-    #             ws.send(json.dumps(move))
-    #         elif 'move_requested' in data:
-    #             # If the server requests a move, send a move.
-    #             move = {'type': 'play', 'color': data['move_requested']['color'], 'vertex': 'D4'}
-    #             ws.send(json.dumps(move))
-
-    #     ws = websocket.WebSocketApp(url, on_message=on_message, header=headers)
-    #     ws.run_forever()
-
-    def print_board(self, board):
-        for i in range(len(board)):
-            print(' '.join(board[i]))
-
-    # def start_game(self, opponent_username):
-    #     self.authenticate()
-    #     game_id = self.send_challenge(opponent_username)
-    #     self.play_game(game_id)
