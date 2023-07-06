8f45d1a9c532e11c5df505f94c4afa000f3db151
dakota.marshall <me@dakotamarshall.net>
2023-07-06 19:15:06 -0400

    test: Move client to test function, and set import test to use local import

--- src/tests/test.py
+++ src/tests/test.py
@@ -5,22 +5,22 @@ from src.ogsapi.client import OGSClient
 class TestOGSClient(unittest.TestCase):
 
     def setUp(self):
-        client_id = os.environ.get('OGS_CLIENT_ID')
-        client_secret = os.environ.get('OGS_CLIENT_SECRET')
-        username = os.environ.get('OGS_USERNAME')
-        password = os.environ.get('OGS_PASSWORD')
-        self.client = OGSClient(client_id, client_secret, username, password)
+        self.client_id = os.environ.get('OGS_CLIENT_ID')
+        self.client_secret = os.environ.get('OGS_CLIENT_SECRET')
+        self.username = os.environ.get('OGS_USERNAME')
+        self.password = os.environ.get('OGS_PASSWORD')
 
     def tearDown(self):
         self.client = None
 
     def test_import(self):
         try:
-            from ogsapi.client import OGSClient
+            from src.ogsapi.client import OGSClient
         except ImportError:
             self.fail("Failed to import OGSClient")
 
     def test_authentication(self):
+        self.client = OGSClient(self.client_id, self.client_secret, self.username, self.password)
         self.assertIsNotNone(self.client.access_token)
         self.assertIsNotNone(self.client.refresh_token)
 
