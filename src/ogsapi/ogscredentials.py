d21d56c6c17dcc044505c9b337225c4adbf31d9d
dakota.marshall <me@dakotamarshall.net>
2023-07-14 00:42:56 -0400

    feat: Add token fields needed for socket authentication

--- src/ogsapi/ogscredentials.py
+++ src/ogsapi/ogscredentials.py
@@ -12,6 +12,9 @@ class OGSCredentials:
         access_token (str, optional): Access token to use for authentication. Defaults to None.
         refresh_token (str, optional): The refresh token to use for authentication. Defaults to None.
         user_id (str, optional): The user ID to use for authentication. Defaults to None.
+        chat_auth (str, optional): The chat auth token to use for authentication. Defaults to None.
+        user_jwt (str, optional): The user JWT to use for authentication. Defaults to None.
+        notification_auth (str, optional): The notification auth token to use for authentication. Defaults to None.
     """
     client_id: str
     client_secret: str
@@ -20,3 +23,6 @@ class OGSCredentials:
     access_token: str = None
     refresh_token: str = None
     user_id: str = None
+    chat_auth: str = None
+    user_jwt: str = None
+    notification_auth: str = None
